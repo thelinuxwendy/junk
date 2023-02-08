6151f88530c53381670866698fa09f0005b6e077
Wim Weber <wim.weber@ip-192-168-12-122.us-west-2.compute.internal>
2023-02-08 16:47:52 -0500

    removing for lab

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#verify first
-certbot certificates
-#renewal next
-#certbot certonly --standalone -d ${DOMAINNAME} --text --agree-tos --email ${ACMEEMAIL} --server https://acme-v02.api.letsencrypt.org/directory --rsa-key-size 4096 --verbose --keep-until-expiring --preferred-challenges=dns
-
